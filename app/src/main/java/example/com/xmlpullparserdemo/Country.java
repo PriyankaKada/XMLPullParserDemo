d8b7483d33a5e422d5a1b7d39c85a95187b42317
Sarika Pandey <sarika.pandey@wwindia.com>
2018-03-26 12:42:32 +0530

    Initial commit

---
+++ app/src/main/java/example/com/xmlpullparserdemo/Country.java
@@ -0,0 +1,33 @@
+package example.com.xmlpullparserdemo;
+
+/**
+ * Created by webwerks on 3/26/18.
+ */
+
+public class Country {
+    public String id,name,capital;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+}
